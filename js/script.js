7aef1f844b0bb613ad1898aae3bce897aab698cd
Rommy Ardiansyah <136146345+LuxiousOfficial@users.noreply.github.com>
2024-02-25 21:21:36 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,26 @@ document.addEventListener('click', function(e) {
 	}
 });
 
+// Togle class active search-form //
+
+	const searchForm = document.querySelector('.search-form');
+	const searchBox = document.querySelector('#search-box');
+
+	document.querySelector('#search-button').onclick = (e) => {
+		searchForm.classList.toggle('active');
+		e.preventDefault();
+	};
+
+	const sb = document.querySelector('#search-button');
+
+	document.addEventListener('click', function(e) {
+		if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+			searchForm.classList.remove('active');
+		}
+	});
+
+// closing Togle class active search-form//
+
 var imageno =1;
 		displayimg(imageno);
 
